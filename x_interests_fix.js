e7f510847ed58521355492ff54c2b2cdc9512682
bozo <bozo@bozohar.com>
2024-11-29 03:05:45 -0800

    Update x_interests_fix.js
    yes i am this dumb

--- x_interests_fix.js
+++ x_interests_fix.js
@@ -1,5 +1,5 @@
 // Find all checkboxes within the div with the specified class
-const checkboxes = document.querySelectorAll('.r-1ye8kvj input[type="checkbox"].r-1ei5mc7');
+const checkboxes = document.querySelectorAll('.r-1ei5mc7');
 
 checkboxes.forEach(checkbox => {
     if (checkbox.checked) {
